5402110043255fc12fda178c660d20a9b8bd3e66
Nik Alekseenko <nukalekseenko@gmail.com>
2023-06-25 18:24:36 +0300

    add simple code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            integers.add(i);
+        }
     }
 }
